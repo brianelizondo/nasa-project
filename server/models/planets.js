3d575560439a196cd0041c1d16089c91171c9fd1
Brian Elizondo <brian_elizondo@msn.com>
2023-02-12 10:36:02 -0500

    NASA Project - Update the server planets model to get all habitable planets from csv file

--- server/models/planets.js
+++ server/models/planets.js
@@ -1,7 +1,9 @@
 /*
 * PLANETS model 
 */
-const planets = [];
+const fs = require("fs");
+const { parse } = require("csv-parse");
+const path = require("path");
 
 /** Handle the errors */
 const {
@@ -9,5 +11,31 @@ const {
     NotFoundError,
 } = require("../expressError");
 
+const planets = [];
 
-module.exports = planets;
+function isHabitablePlanet(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED'
+      && planet['koi_insol'] > 0.36 
+      && planet['koi_insol'] < 1.11
+      && planet['koi_prad'] < 1.6;
+} 
+
+fs.createReadStream(path.join(__dirname, '..', 'data', 'kepler_data.csv'))
+    .pipe(parse({
+        comment: '#',
+        columns: true,
+    }))
+    .on('data', (data) => {
+        if(isHabitablePlanet(data)) {
+            planets.push(data);
+        }
+    })
+    .on('error', (err) => {
+        console.log(err);
+    }
+);
+
+
+module.exports = {
+    planets
+};
