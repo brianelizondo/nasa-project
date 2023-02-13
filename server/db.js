6308d90d5e0a55ec86dabc4f6466856117e6df79
Brian Elizondo <brian_elizondo@msn.com>
2023-02-13 14:33:36 -0500

    NASA Project - Server setup files and config connect the app to the MongoDB atlas cluster with mongoose

---
+++ server/db.js
@@ -0,0 +1,19 @@
+/**
+* Connect to MongoDB using mongoose
+*/
+require('dotenv').config();
+const MONGODB_URL = process.env.MONGODB_URL;
+
+const mongoose = require('mongoose');
+// alert success connection
+mongoose.connection.once('open', () => {
+    console.log("MongoDB connection is ready!");
+});
+// start MongoDB connection
+async function connectToMongoDB(){
+    await mongoose.connect(MONGODB_URL);
+}
+
+module.exports = {
+    connectToMongoDB
+}
--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@
 */
 const app = require('./app');
 const PORT = process.env.PORT || 3001;
+const { connectToMongoDB } = require("./db");
+
+// start connect to MongoBD database
+connectToMongoDB();
 
 app.listen(PORT, function () {
     console.log(`Server starting on port ${PORT}`);
